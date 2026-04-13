53c6a64465476c0a290f1a99934266a8607096f9
Stephen Rhodes <sr99622@gmail.com>
2026-04-13 16:07:30 -0400

    windows FAFO

--- kankakee/__init__.py
+++ kankakee/__init__.py
@@ -1,14 +1,17 @@
-from importlib.metadata import version, PackageNotFoundError
-import sys
+from .kankakee import *
 
-if sys.platform == "win32":
-    from kankakee import *
-    from kankakee import __doc__
-else:
-    from .kankakee import *
-    from .kankakee import __doc__
 
-try:
-    __version__ = version("kankakee")
-except PackageNotFoundError:
-    __version__ = "unknown"
+#from importlib.metadata import version, PackageNotFoundError
+#import sys
+#
+#if sys.platform == "win32":
+#    from kankakee import *
+#    from kankakee import __doc__
+#else:
+#    from .kankakee import *
+#    from .kankakee import __doc__
+#
+#try:
+#    __version__ = version("kankakee")
+#except PackageNotFoundError:
+#    __version__ = "unknown"
