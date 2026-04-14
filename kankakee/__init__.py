c354f15414e10929a4a5e0c39a3eefb30d089afd
Stephen Rhodes <sr99622@gmail.com>
2026-04-14 17:17:22 +0000

    working linux build

--- kankakee/__init__.py
+++ kankakee/__init__.py
@@ -1,17 +1,15 @@
-from .kankakee import *
+from importlib.metadata import version, PackageNotFoundError
+import sys
 
+if sys.platform == "win32":
+    from kankakee import *
+    from kankakee import __doc__
+else:
+    from .kankakee import *
+    from .kankakee import __doc__
 
-#from importlib.metadata import version, PackageNotFoundError
-#import sys
-#
-#if sys.platform == "win32":
-#    from kankakee import *
-#    from kankakee import __doc__
-#else:
-#    from .kankakee import *
-#    from .kankakee import __doc__
-#
-#try:
-#    __version__ = version("kankakee")
-#except PackageNotFoundError:
-#    __version__ = "unknown"
+try:
+    __version__ = version("kankakee")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
