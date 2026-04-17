5f802a7fcf0403e28f84377f5c7f363d82802877
Stephen Rhodes <sr99622@gmail.com>
2026-04-16 23:49:15 -0400

    command line version of server

---
+++ examples/cmd_server.py
@@ -0,0 +1,38 @@
+from kankakee import Server
+import numpy as np
+import io
+from loguru import logger
+import traceback
+
+class ServerProtocols():
+    def __init__(self, mo):
+        self.mw = mo
+
+    def callback(self, msg):
+        if msg == "QUIT":
+            self.mo.server.stop()
+        buffer = io.BytesIO()
+        buffer.write(bytearray(msg, 'utf-8'))
+        buffer.write(bytearray("\r\n", 'utf-8'))
+        return np.frombuffer(buffer.getvalue(), dtype=np.uint8)
+    
+    def error(self, msg):
+        logger.error(f"server protocol error: {msg}")
+        logger.debug(traceback.format_exc())
+
+class MainObject():
+    def __init__(self):
+        super().__init__()
+        self.server = None
+        self.serverProtocols = ServerProtocols(self)
+
+        self.server = Server("127.0.0.1", 8000)
+        self.server.serverCallback = self.serverProtocols.callback
+        self.server.errorCallback = self.serverProtocols.error
+        self.server.start()
+
+if __name__ == "__main__":
+    mo = MainObject()
+    while True:
+        ...
+    
